e366f969c285bbb10c9fa6a408551adaf1c391bd
IntnAmi <100425887+IntnAmi@users.noreply.github.com>
2022-09-24 22:00:09 +0700

    Add files via upload

---
+++ Sesi1/Program2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesi1PBO;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Program2 {
+    public static void main(String[] args) {
+       String kalimat=JOptionPane.showInputDialog("Anda sedang belajar apa?");
+       JOptionPane.showMessageDialog(null," Belajar " + kalimat + " Sangat menyenangkan ");
+    }
+}
